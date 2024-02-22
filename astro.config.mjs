dc2b5d6a9bb738436c8ca1447f74f697d91349fb
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-22 10:29:15 +0100

    fix: astro image config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })],
-  output: "server",
-  adapter: vercel()
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	output: 'server',
+	adapter: vercel(),
+	image: {
+		service: passthroughImageService(),
+	},
 });

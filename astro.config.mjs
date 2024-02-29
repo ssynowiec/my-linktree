a292578e658753584c377195ba2c26a934b2b371
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-29 16:49:51 +0100

    add: patytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import vercel from '@astrojs/vercel/serverless';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), partytown()],
   output: 'server',
   adapter: vercel(),
   image: {
-    service: passthroughImageService(),
-  },
+    service: passthroughImageService()
+  }
 });

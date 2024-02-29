514ccfba2135869e40ff22efc0fe2a1472f46bb8
Stanisław Synowiec <stasyn1410@gmail.com>
2024-02-29 16:52:50 +0100

    add: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,24 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), partytown(), sitemap()],
+  site: 'https://links.ssynowiec.pl',
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    partytown(),
+    sitemap(),
+  ],
   output: 'server',
   adapter: vercel(),
   image: {
-    service: passthroughImageService()
-  }
+    service: passthroughImageService(),
+  },
 });
